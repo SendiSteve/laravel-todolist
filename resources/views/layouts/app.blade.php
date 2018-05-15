47317467272d041cc5ab06fe7db6a056c20ba995
SendiSteve <sendi.stev@gmail.com.com>
2018-05-15 12:19:11 +0300

    Create layout file for default html

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo-list</title>
+
+    {{-- custom css --}}
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <footer id="footer">
+        <p>Copyright &copy; 2018. Developed by Stephen</p>
+    </footer>
+    
+</body>
+</html>
